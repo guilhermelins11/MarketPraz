b62bd1c6c492bb90057f3cc57e3d8e0098162036
Antony Guilherme <contato.antonyguilherme@gmail.com>
2025-05-29 22:57:18 -0300

    Delete projeto_crud/src directory

--- projeto_crud/src/main/java/com/example/projeto_crud/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.projeto_crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.projeto_crud.model.Usuario; // Import necessário
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Métodos CRUD básicos já estão implementados pelo JpaRepository
-}
